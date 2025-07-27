a513143ce80877a045328698d28aea53f8f1d706
YongChenSu <yongchensu0@gmail.com>
2025-07-27 23:38:49 +0800

    chore(example): Update target Instagram username in example.py

--- instagram_posts_scraper/example.py
+++ instagram_posts_scraper/example.py
@@ -2,8 +2,7 @@
 #%%
 from instagram_posts_scraper.instagram_posts_scraper import InstaPeriodScraper
 
-
-target_info = {"username": "kaicenat", "days_limit":5}
+target_info = {"username": "mapleshadow18", "days_limit":5}
 ig_posts_scraper = InstaPeriodScraper()
 res = ig_posts_scraper.get_posts(target_info=target_info)
 res
